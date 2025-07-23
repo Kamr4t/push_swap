b234642cb2389dc7aadebbdede65e65cdaa62ab0
Kamr4t <anthony.camara.ac@googlemail.com>
2025-07-23 11:40:39 +0200

    trying to splitt a lot in different functions

--- sort.c
+++ sort.c
@@ -6,68 +6,103 @@
 /*   By: ancamara <ancamara@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/20 11:11:50 by ancamara          #+#    #+#             */
-/*   Updated: 2025/07/22 16:42:13 by ancamara         ###   ########.fr       */
+/*   Updated: 2025/07/23 11:00:41 by ancamara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-//remove the insert array to another function
-//also removed used slots
-static int	find_next_index(int *array, int index)
+static void	shift_index_array(int *array, int pos, int index, int used_slots)
+{
+	int	tmp;
+
+	while (pos < used_slots)
+	{
+		tmp = array[pos];
+		array[pos] = index;
+		index = tmp;
+		pos++;
+	}
+}
+
+static int	find_next_index(int *array, int index, bool shift_array)
 {
 	int			i;
-	int			tmp;
 	int			next_index;
 	static int	used_slots = 0;
-	int			flag;
+	bool		is_zero;
 
 	i = 0;
-	tmp = 0;
 	next_index = 0;
-	flag = 0;
+	is_zero = false;
 	while (i < used_slots)
 	{
 		if (index > array[i])
 		{
 			next_index = array[i];
-			flag = 1;
+			is_zero = true;
 			break ;
 		}
 		i++;
 	}
 	used_slots++;
-	while (i < used_slots)
+	if (next_index == 0 && !is_zero)
 	{
-		tmp = array[i];
-		array[i] = index;
-		index = tmp;
-		i++;
-	}
-	if (next_index == 0 && flag == 0)
+		if (shift_array)
+			shift_index_array(array, i, array[0], used_slots);
 		return (array[0]);
+	}
+	if (shift_array)
+		shift_index_array(array, i, index, used_slots);
 	return (next_index);
 }
 
-//bullshit adding it to b lst doesnt help at all
-//needs to return the amount of operation needed and add it in the main function to the a lst
-//if directions are the same return the difference
-static int	add_operation_count_b(t_data ***data, int index)
+static void	add_operation_count_b(t_data ***data, int index, int lst_a_nbr)
 {
 	int		i;
-	stack	*lst;
+	stack	*lst_a;
+	stack	*lst_b;
 
 	i = 0;
-	lst = *((*(*data))->lst_b);
-	if (!lst)
+	lst_a = *((***data).lst_a);
+	lst_b = *((***data).lst_b);
+	if (!lst_b)
 		return ;
-	while (lst->index != index)
+	while (lst_b->index != index)
 	{
-		lst = lst->next;
+		lst_b = lst_b->next;
 		i++;
 	}
-	//read lst a for dircetion and operations
-	//do the math edit the lst a
+	while (lst_a->nbr != lst_a_nbr)
+		lst_a = lst_a->next;
+	if (i < (***data).len_b / 2)
+	{
+		if (lst_a->direction == 1)
+		{
+			lst_a->operations -= i;
+			if (lst_a->operations < 0)
+				lst_a->operations = - lst_a->operations;
+		}
+		else
+		{
+			lst_a->operations += i;
+			lst_a->operations = 0;
+		}
+	}
+	else
+	{
+		if (lst_a->direction == -1)
+		{
+			lst_a->operations -= i;
+			if (lst_a->operations < 0)
+				lst_a->operations = - lst_a->operations;
+		}
+		else
+		{
+			lst_a->operations += i;
+			lst_a->operations = 0;
+		}
+	}
 }
 
 static void	add_operation_count(t_data **data, int *array)
@@ -76,6 +111,7 @@ static void	add_operation_count(t_data **data, int *array)
 	stack	*lst;
 
 	i = 0;
+	(void)array;
 	lst = *((*data)->lst_a);
 	while (lst)
 	{
@@ -89,16 +125,33 @@ static void	add_operation_count(t_data **data, int *array)
 			(*(*(*data)->lst_a)).operations = (*data)->len_a - i;
 			(*(*(*data)->lst_a)).direction = -1;
 		}
-		add_operation_count_b(&data, find_next_index(array, lst->index));
+		add_operation_count_b(&data, find_next_index(array, lst->index, false), lst->nbr);
 		lst = lst->next;
 		i++;
 	}
 }
 
-static int	compare_operations(t_data **data)
+static void	move_node(t_data **data, int *array)
 {
-	int	index;
-	
+	stack	*lst;
+	int		op_count;
+
+	op_count = 0;
+	lst = *((*data)->lst_a);
+	op_count = lst->operations;
+	while (lst)
+	{
+		lst = lst->next;
+		if (op_count > lst->operations)
+			op_count = lst->operations;
+	}
+	lst = *((*data)->lst_a);
+	while (lst->operations != op_count)
+		lst = lst->next;
+	while ((*(*(*data)->lst_a)).index != lst->index)
+	{
+		if (lst->direction == 1)
+	}
 }
 
 //sort function 
@@ -109,7 +162,7 @@ static int	compare_operations(t_data **data)
 void	main_sort(t_data *data)
 {
 	int		*array;
-	int		next_index;
+	//int		next_index;
 	stack	*lst_a;
 
 	lst_a = *(data->lst_a);
@@ -118,11 +171,13 @@ void	main_sort(t_data *data)
 		return ;
 	while (data->len_a > 0)
 	{
+		//ft_push(&(data->lst_b), &(data)->lst_a);
 		add_operation_count(&data, array);
 		// next_index = find_next_index(array, lst_a->index);
 		// rotate_to_index(&data, next_index);
 		// ft_push(&(data->lst_b), &(data)->lst_a);
-		// data->len_a--;
-		// lst_a = *(data->lst_a);
+		data->len_a--;
+		lst_a = *(data->lst_a);
+		break ;
 	}
 }
